90db2c419cc48c79ed97b95f18d55c84ca1eca80
Tommax2 <martinsolumi@gmail.com>
2025-12-20 12:30:00 +0100

    comm

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send('Signup Route is called');
+}
+
+export const login = async (req, res) => {
+    res.send('Login Route is called');
+}
+
+export const logout = async (req, res) => {
+    res.send('Logout Route is called');
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
-const app = express();
-const port = 5000;
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import { connect } from 'mongoose';
+import { connectDB } from '../lib/db.js';
 
-app.listen(5000, () => {  
-    console.log('Server is running on port 5000');  
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {  
+    console.log('Server is running Http://localhost:' + PORT); 
+    
+    connectDB();
 });  
