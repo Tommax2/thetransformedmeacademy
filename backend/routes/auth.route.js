5a8325922e0d6297d41498ec40c2b6d6aede5674
Tommax2 <martinsolumi@gmail.com>
2025-12-27 15:38:53 +0100

    add

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,8 @@
 
 import express from 'express';
-import { signup, login, logout, refreshToken } from '../controllers/auth.controller.js'; 
+import { signup, login, logout, refreshToken, getProfile,  } from '../controllers/auth.controller.js'; 
 import { get } from 'mongoose';
+import { protectRoute } from '../middleware/auth.middle.js';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.post('/signup',signup);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/refresh-token', refreshToken);
-// router.post('/profile',getProfile);
+ router.get ('/profile', protectRoute, getProfile);
 
 export default router;
 
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
 import paymentRoutes from './routes/payment.route.js';
+import analyticsRoutes from './routes/analytics.route.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use('/api/payment', paymentRoutes);
+app.use('/api/analytics', analyticsRoutes);
 
 app.listen(PORT, () => {  
     console.log('Server is running Http://localhost:' + PORT); 
